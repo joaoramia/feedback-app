dd2b8690ac5823cf8c5992aec81b2c922109f672
joaoramia <joao.ramia@gmail.com>
2019-06-12 21:06:02 +0200

    including link bar

--- client/lib/link/index.js
+++ client/lib/link/index.js
@@ -2,25 +2,24 @@ window.onload = function(d) {
     var iframe = document.createElement('iframe');
     win = iframe.contentWindow || iframe;
     iframe.style.border = 'none';
-    iframe.style.height = '250px';
-    iframe.style.width = '250px';
+    iframe.style.height = '52px';
+    iframe.style.width = '100%';
     iframe.style.position = 'absolute';
     iframe.style.top = '0';
-    iframe.style.bottom = '0';
-    iframe.style.margin = 'auto';
     iframe.src = 'http://localhost:8080/cdn/link/link.html?token=' + window.parent.feedbackApp.widget;
 
     document.body.appendChild(iframe);
+    createPusher();
   };
   
   function createPusher() {
     var pusher = document.createElement('div');
     win = pusher.contentWindow || pusher;
     pusher.style.border = 'none';
-    pusher.style.height = '100px';
+    pusher.style.height = '52px';
     pusher.style.width = '100%';
     pusher.style.position = 'relative';
     pusher.style.flex = 'none';
     pusher.style.overflow = 'hidden';
-    document.body.appendChild(pusher);
+    document.body.insertBefore(pusher, document.body.firstChild);
   }
